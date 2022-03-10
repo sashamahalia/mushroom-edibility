586c8be0da1dcb44251f30e15cbd167b37431ac4
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2022-03-09 16:01:42 -0800

    Add human readable label

--- knn.py
+++ knn.py
@@ -17,13 +17,18 @@ cap_color = le.fit_transform(list(data["cap-color"]))
 odor = le.fit_transform(list(data["odor"]))
 bruises = le.fit_transform(list(data["bruises"]))
 population = le.fit_transform(list(data["population"]))
-habitat = le.fit_transform(list(data["habitat"]))
 stalk_shape = le.fit_transform(list(data["stalk-shape"]))
 gill_color = le.fit_transform(list(data["gill-color"]))
 
 # setup input and output
-X = list(zip(cap_shape, cap_color, odor, bruises, population, habitat, stalk_shape, gill_color))
+X = list(zip(cap_shape, cap_color, odor, bruises, population, stalk_shape, gill_color))
 y = list(edibility)
+
+# Adds human readable label for output
+labels = {0 : 'edible',
+              1 : 'poisonous'}
+
+y = np.vectorize(labels.__getitem__)(y)
 
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
 
@@ -32,6 +37,7 @@ knn = KNeighborsClassifier(n_neighbors=1)
 # fit the model
 knn.fit(X_train, y_train)
 predictions = knn.predict(X_test)
+
 acc = accuracy_score(y_test, predictions)
 print(f'Successfully trained model with an accuracy of {acc:.2f}')
 
