52baf434b758efdbb098c6b841bee38f1da0d375
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2022-03-11 17:01:20 -0800

    Create flask api for submitting user attributes to predict edibility

--- knn.py
+++ knn.py
@@ -5,6 +5,7 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 from sklearn import linear_model, preprocessing
+import pickle
 
 data = pd.read_csv("mushroom-data/agaricus-lepiota.data")
 
@@ -25,24 +26,29 @@ X = list(zip(cap_shape, cap_color, odor, bruises, population, stalk_shape, gill_
 y = list(edibility)
 
 # Adds human readable label for output
-labels = {0 : 'edible',
-              1 : 'poisonous'}
+labels = {0: 'edible',
+          1: 'poisonous'}
 
 y = np.vectorize(labels.__getitem__)(y)
 
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
 
-knn = KNeighborsClassifier(n_neighbors=1)
+def train(X, y):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
 
-# fit the model
-knn.fit(X_train, y_train)
-predictions = knn.predict(X_test)
+    knn = KNeighborsClassifier(n_neighbors=1)
 
-acc = accuracy_score(y_test, predictions)
-print(f'Successfully trained model with an accuracy of {acc:.2f}')
+    # fit the model
+    knn.fit(x_train, y_train)
+    predictions = knn.predict(x_test)
+
+    acc = accuracy_score(y_test, predictions)
+    print(f'Successfully trained model with an accuracy of {acc:.2f}')
+    return knn
 
 
+if __name__ == '__main__':
+    model = train(X, y)
 
-
-
-
+    # serialize model
+    with open("mushrooms.pickle", "wb") as f:
+        pickle.dump(model, f)
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import pickle
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+APP = Flask(__name__)
+API = Api(APP)
+
+pickle_in = open("mushrooms.pickle", "rb")
+MUSHROOM_MODEL = pickle.load(pickle_in)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Predict(Resource):
 
+    @staticmethod
+    def post():
+        parser = reqparse.RequestParser()
+        parser.add_argument('cap_shape')
+        parser.add_argument('cap_color')
+        parser.add_argument('odor')
+        parser.add_argument('bruises')
+        parser.add_argument('population')
+        parser.add_argument('stalk_shape')
+        parser.add_argument('gill_color')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        args = parser.parse_args()  # creates dict
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        X_new = np.fromiter(args.values(), dtype=float)  # convert input to array
+
+        out = {'Prediction': MUSHROOM_MODEL.predict([X_new])[0]}
+
+        return out, 200
